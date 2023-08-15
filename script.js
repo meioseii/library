f98515b6413fd3bc1b85219b6b413fe48b3a3721
meioseii <moreno.austin12@gmail.com>
2023-08-15 15:51:24 +0800

    added book constructor and librarary

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const dialog = document.querySelector('#form-dialog');
 const form = document.querySelector('form');
 const closeDialogBtn = document.querySelector('.cancel');
 const openDialogBtn = document.querySelector('.new-book');
+const addBook = document.querySelector('.submit');
+
+const myLibrary = [];
 
 openDialogBtn.addEventListener('click', () => {
+    form.reset();
     dialog.showModal();
 })
 
@@ -13,3 +17,22 @@ closeDialogBtn.addEventListener('click', (event) => {
     form.reset();
 })
 
+addBook.addEventListener('click', addBookToLibrary);
+
+function Book(title, author, pages, status) {
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.status = status
+}
+
+function addBookToLibrary() {
+  const bookTitle = document.getElementById('book-title').value;
+  const bookAuthor = document.getElementById('book-author').value;
+  const bookPages = document.getElementById('book-pages').value;
+  const bookStatus = document.getElementById('book-status').checked;
+
+  const book = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+
+  bookTitle && bookAuthor && bookPages !== "" ? myLibrary.push(book) : null;
+}
